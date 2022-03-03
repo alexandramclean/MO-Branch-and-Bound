a8a714e0ffc846c353f156304848e6efd0e4b026
alexandramclean <mcleanalexandra018@gmail.com>
2022-03-03 15:20:44 +0100

    Merged test files

--- parametricTest.jl
+++ parametricTest.jl
@@ -1,35 +1,12 @@
 ################################################################################
 #         Stage M2 - Ensembles bornants pour un B&B multi-objectif             #
 #         MCLEAN Alexandra, encadré par Gandibleux X. et Przybylski A.         #
-#         Calcul paramétrique de la relaxation continue - Tests                #
+#         Calcul paramétrique - Tests                                          #
 ################################################################################
 
 include("lpRelaxation.jl")
-
+include("martelloAndToth.jl")
 using Test 
-
-# ---------------------------------------------------------------------------- #
-function testRatios()
-	didactic = _MOMKP([11 2 8 10 9 1 ; 2 7 8 4 1 3], [4 4 6 4 3 2], [11])
-	r1, r2 = ratios(didactic) 
-	
-	@testset "Ratio Tests" begin 
-		@test r1 == [11//4, 1//2, 4//3, 5//2, 3//1, 1//2]
-		@test r2 == [1//2, 7//4, 4//3, 1//1, 1//3, 3//2]
-	end; 
-end 
-
-# ---------------------------------------------------------------------------- #
-function testCriticalWeights() 
-	didactic = _MOMKP([11 2 8 10 9 1 ; 2 7 8 4 1 3], [4 4 6 4 3 2], [11])
-	r1, r2 = ratios(didactic) 
-	weights, pairs = criticalWeights(didactic, r1, r2) 
-	
-	@testset "Critical Weights | Didactic" begin 
-		@test pairs == [(1,4), (4,5), (1,5), (3,5), (1,3), (2,5), (1,2), (2,3), 
-						(5,6), (1,6), (2,4), (3,4), (4,6), (3,6)] 
-	end
-end
 
 # ---------------------------------------------------------------------------- #
 function testTranspositions() 
@@ -52,7 +29,7 @@ function testReoptSolution()
 	prev = [5,1,4,3,2,6] 
 	seq  = [5,3,4,1,2,6] 
 	sol  = Solution([1,0,0,1,1,0], [30,7]) 
-	sol, s, residualCapacity = reoptSolution(didactic, prev, seq, 2, 4, sol, 0)
+	sol, s, residualCapacity = reoptSolution(didactic, seq, 2, 4, sol, 0)
 	
 	@testset "Reoptimisation Tests" begin 
 		@test sol.X == [0,0,1,1//2,1,0]
@@ -61,7 +38,7 @@ function testReoptSolution()
 		@test s == 3
 	end;
 	
-	sol, s, residualCapacity = reoptSolution(didactic, seq, prev, 2, s, sol, residualCapacity)
+	sol, s, residualCapacity = reoptSolution(didactic, prev, 2, s, sol, residualCapacity)
 	
 	@testset "Reoptimisation Tests 2" begin 
 		@test sol.X == [1,0,0,1,1,0]
@@ -76,7 +53,7 @@ function testReoptSolution()
 	
 	fname = "../instancesPG/set2/D4.DAT" 
 	prob  = readInstanceMOMKPformatPG(false, fname)
-	sol, s, residualCapacity = reoptSolution(prob, prev, seq, 18, 19, sol, 29)
+	sol, s, residualCapacity = reoptSolution(prob, seq, 18, 19, sol, 29)
 	
 	@testset "Reoptimisation Tests 3" begin 
 		@test sol.z == [135039//133, 155049//133]
@@ -93,6 +70,44 @@ function testSetVariable()
 	println(newPos)
 end
 	
+# ---------------------------------------------------------------------------- #
+function testChooseBound()
+	# Exemple didactique
+	prob = _MOMKP([11 2 8 10 9 1 ; 2 7 8 4 1 3], [4 4 6 4 3 2], [11])
+	
+	r1, r2 = ratios(prob)
+	weights, pairs = criticalWeights(prob, r1, r2) 
+	 
+	upperBound = Vector{Float64}[]
+	constraints = Constraint[]
+	
+	@testset "chooseBound Tests" begin 
+	
+		# Cas λeq plus petit que le λ critique suivant 
+		U0 = [30.0, 7.0]
+		U1 = [23.0, 9.0]
+		chooseBound!(upperBound, constraints,1//1, weights[1], U0, U1)
+		@test upperBound == [[30.0, 7.0]] 
+		
+		# Cas λeq égal au λ critique suivant
+		U0 = [30.0, 7.0]
+		U1 = [20.0, 13.0]
+		chooseBound!(upperBound, constraints, weights[3], weights[4], U0, U1)
+		@test upperBound == [[30.0, 7.0]]
+		
+		# Cas λeq strictement compris entre les λ critiques précédent et suivant
+		U0 = [30.0, 7.0]
+		U1 = [83/4, 25/2]
+		chooseBound!(upperBound, constraints, weights[4], weights[5], U0, U1)
+		@test upperBound == [[83/4, 25/2], [30.0, 7.0]]
+		
+		
+		U0 = [37/2, 55/4] 
+		U1 = [25.0, 10.0] 
+		chooseBound!(upperBound, constraints, weights[6], weights[7], U0, U1)
+		@test upperBound == [[37/2, 55/4], [83/4, 25/2], [30.0, 7.0]]
+	end;
+end 
 
 
 
