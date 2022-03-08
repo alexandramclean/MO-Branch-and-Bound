c0c92d81278fbca24a3d26031e01841bde70fa53
alexandramclean <mcleanalexandra018@gmail.com>
2022-03-08 17:37:27 +0100

    Adapted ordered list code to contain solutions instead of points

--- listeOrdonnee.jl
+++ listeOrdonnee.jl
@@ -10,8 +10,8 @@ using Random
 
 ## VERIFIER
 # Recherche de l'indice du dernier point dominé par y
-function dernier_point_domine(yN,
-                              y,
+function dernier_point_domine(yN::Vector{Solution},
+                              y::Solution,
                               deb::Int64, fin::Int64,
                               opt::Optimisation=MAX) 
     if deb >= fin
@@ -45,8 +45,9 @@ end
 
 # Vérification que les successeurs de y (>ind) ne sont pas dominés par y
 # Si y domine des points de yN alors il domine sont successeur large minimum
-function verifier(yN,
-                  y, ind::Int64,
+function verifier(yN::Vector{Solution},
+                  y::Solution, 
+                  ind::Int64,
                   opt::Optimisation=MAX) 
                   
     if opt == MIN && ind < length(yN) && domine(y, yN[ind+1], opt)
@@ -66,15 +67,15 @@ function verifier(yN,
 end
 
 ## AJOUTER
-function ajouter_rec(yN,
-                     y,
+function ajouter_rec(yN::Vector{Solution},
+                     y::Solution,
                      deb::Int64, fin::Int64,
                      opt::Optimisation=MAX)
                      
     if deb >= fin # Cas d'arrêt : 1 seul élément dans la sous-liste
         if domine(yN[deb], y, opt)
             return 0
-        elseif yN[deb][1] < y[1]
+        elseif yN[deb].z[1] < y.z[1]
             return deb + 1
         else
             return deb
@@ -86,7 +87,7 @@ function ajouter_rec(yN,
     # Si y est dominé par un point dans la liste on ne l'insère pas
     if domine(yN[mil], y, opt) || domine(yN[deb], y, opt) || domine(yN[fin], y, opt)
         return 0
-    elseif yN[mil][1] > y[1] 
+    elseif yN[mil].z[1] > y.z[1] 
         # Le cas d'égalité sur les deux fonctions objectif a déjà été traité
         # dans le test de dominance
         return ajouter_rec(yN, y, deb, mil-1, opt)
@@ -95,7 +96,9 @@ function ajouter_rec(yN,
     end
 end
 
-function ajouter!(yN, y, opt::Optimisation=MAX)
+function ajouter!(yN::Vector{Solution}, 
+                  y::Solution, 
+                  opt::Optimisation=MAX)
     #! Affichage
     #println("Ajout de ", y)
 
@@ -121,22 +124,22 @@ end
 function afficher(yN)
     print("|  ")
     for i in 1:length(yN)
-        if yN[i][1] < 10
-            print("  ", yN[i][1], "  ")
-        elseif yN[i][1] < 100
-            print(" ", yN[i][1], "  ")
+        if yN[i].z[1] < 10
+            print("  ", yN[i].z[1], "  ")
+        elseif yN[i].z[1] < 100
+            print(" ", yN[i].z[1], "  ")
         else
-            print(yN[i][1], "  ")
+            print(yN[i].z[1], "  ")
         end
     end
     print("|\n| ")
     for i in 1:length(yN)
-        if yN[i][2] < 10
-            print("  ", yN[i][2], "  ")
-        elseif yN[i][2] < 100
-            print(" ", yN[i][2], "  ")
+        if yN[i].z[2] < 10
+            print("  ", yN[i].z[2], "  ")
+        elseif yN[i].z[2] < 100
+            print(" ", yN[i].z[2], "  ")
         else
-            print(yN[i][2], "  ")
+            print(yN[i].z[2], "  ")
         end
     end
     print("|\n")
