43308e075bff15305702dd060800676f3795c31d
Alexandra McLean <52014131+alexandramclean@users.noreply.github.com>
2022-05-17 01:18:03 +0200

    Cleanup + added comments

--- orderedList.jl
+++ orderedList.jl
@@ -21,8 +21,8 @@ end
 
 # ----- VERIFY --------------------------------------------------------------- #
 # Searches for the index of the last point dominated by y 
-function lastDominatedPoint(yN::Union{Vector{Solution{T}}, Vector{Vector{T}}},
-                            y::Union{Solution{T}, Vector{T}},
+function lastDominatedPoint(yN::Vector{Vector{T}},
+                            y::Vector{T},
                             start::Int64, 
                             finish::Int64,
                             opt::Optimisation=MAX) where T<:Real
@@ -61,9 +61,7 @@ function lastDominatedPoint(yN::Union{Vector{Solution{T}}, Vector{Vector{T}}},
     end
 end
 
-# Verifies that y's successors (>ind) are not dominated by y 
-# Vérification que les successeurs de y (>ind) ne sont pas dominés par y
-# Si y domine des points de yN alors il domine sont successeur large minimum
+# Verifies that y's successors (>ind if MIN or <ind if MAX) are not dominated 
 function verify(yN::Union{Vector{Solution{T}}, Vector{Vector{T}}},
                 y::Union{Solution{T}, Vector{T}}, 
                 ind::Int64,
@@ -73,16 +71,34 @@ function verify(yN::Union{Vector{Solution{T}}, Vector{Vector{T}}},
 
     # If y dominates any points it dominates its immediate successor 
     # (or predecessor if opt == MAX)
-    if opt == MIN && ind < length(yN) && dominates(y, yN[ind+1], opt) 
-        indLastDominated = lastDominatedPoint(yN, y, ind+1, length(yN), opt)
 
+    if opt == MIN && ind < length(yN) && dominates(y, yN[ind+1], opt) 
+
+        # The ordered list contains solutions 
+        if typeof(y) == Solution{Float64} || typeof(y) == Solution{Rational{Int}}
+            indLastDominated = lastDominatedPoint([sol.z for sol in yN], y.z, ind+1, length(yN), opt)
+        else 
+            # The ordered list contains points 
+            indLastDominated = lastDominatedPoint(yN, y, ind+1, length(yN), opt)
+        end 
+
+        # Deletion of the dominated points 
         for j in indLastDominated:-1:ind+1
             deleteat!(yN, j)
         end
+
     elseif opt == MAX && ind > 1 && dominates(y, yN[ind-1], opt)
         # First dominated point
-        indLastDominated = lastDominatedPoint(yN, y, 1, ind-1, opt)
 
+        # The ordered list contains solutions 
+        if typeof(y) == Solution{Float64} || typeof(y) == Solution{Rational{Int}}
+            indLastDominated = lastDominatedPoint([sol.z for sol in yN], y.z, 1, ind-1, opt)
+        else 
+            # The ordered list contains points 
+            indLastDominated = lastDominatedPoint(yN, y, 1, ind-1, opt)
+        end 
+
+        # Deletion of the dominated points 
         for j in ind-1:-1:indLastDominated
             deleteat!(yN, j)
         end 
@@ -90,8 +106,11 @@ function verify(yN::Union{Vector{Solution{T}}, Vector{Vector{T}}},
 end
 
 # ----- ADD ------------------------------------------------------------------ #
-function addRec(yN::Union{Vector{Solution{T}}, Vector{Vector{T}}},
-                y::Union{Solution{T}, Vector{T}},
+# Returns the index of y so as to maintain an ordered list 
+# If the index returned is 0 then y should not be inserted (dominated point or 
+# duplicate)
+function addRec(yN::Vector{Vector{T}},
+                y::Vector{T},
                 start::Int64, 
                 finish::Int64,
                 opt::Optimisation=MAX) where T<:Real
@@ -112,7 +131,8 @@ function addRec(yN::Union{Vector{Solution{T}}, Vector{Vector{T}}},
             return 0 
 
         # There is no dominance between y and yN[start]
-        elseif isStrictlySmaller(y, yN[start], 1)
+        elseif y[1] < yN[start][1] 
+            #!isStrictlySmaller(y, yN[start], 1)
             # y is inserted before yN[start]
             return start 
 
@@ -129,7 +149,8 @@ function addRec(yN::Union{Vector{Solution{T}}, Vector{Vector{T}}},
         || dominates(yN[finish], y, opt))
         return 0
 
-    elseif isStrictlySmaller(y, yN[mid], 1)
+    elseif y[1] < yN[mid][1] 
+        #!isStrictlySmaller(y, yN[mid], 1)
         # The case where y and yN[mid] have equal values for both objective 
         # functions is included in the dominance test 
         return addRec(yN, y, start, mid-1, opt)
@@ -138,78 +159,65 @@ function addRec(yN::Union{Vector{Solution{T}}, Vector{Vector{T}}},
     end
 end
 
+# Adds an element to an ordered list 
+# The ordered list can be an array of arrays (points) or an array 
+# of solutions
 function add!(yN::Union{Vector{Solution{T}}, Vector{Vector{T}}}, 
               y::Union{Solution{T},Vector{T}}, 
               opt::Optimisation=MAX) where T<:Real
 
+    # The ordered list contains solutions 
     if typeof(y) == Solution{Float64} || typeof(y) == Solution{Rational{Int}}
+        #! Should not be needed 
         y.z = [floor(y.z[1]), floor(y.z[2])]
+
+        # Search for the position of y 
+        if length(yN) == 0
+            insert!(yN, 1, y)
+
+        else
+            # addRecc only accepts an array of points 
+            ind = addRec([sol.z for sol in yN], y.z, 1, length(yN), opt)
+            if ind > 0 
+                insert!(yN, ind, y)
+
+                # Elimination of the dominated points 
+                verify(yN, y, ind, opt)
+            end 
+        end 
+
+
     else 
+        # The ordered list contains points 
+        #! Should not be required 
         y = [floor(y[1]), floor(y[2])]
     end 
     
-    # Search for the position of y 
-    if length(yN) == 0
-        insert!(yN, 1, y)
-
-    else
-        ind = addRec(yN, y, 1, length(yN), opt)
-        if ind > 0 
-            insert!(yN, ind, y)
-
-            # Elimination of the dominated points 
-            verify(yN, y, ind, opt)
-        end 
-    end 
+    
 end
 
-# ----- AFFICHER ------------------------------------------------------------- #
+# ----- PRINT ---------------------------------------------------------------- #
 # Prints yN in a readable format
-function afficher(yN)
-    if typeof(yN[1]) == Solution{Float64} || 
-            typeof(yN[1]) == Solution{Rational{Int}}
-        print("| ")
-        for i in 1:length(yN)
-            if yN[i].z[1] < 10
-                print("  ", yN[i].z[1], "  ")
-            elseif yN[i].z[1] < 100
-                print(" ", yN[i].z[1], "  ")
-            else
-                print(yN[i].z[1], "  ")
-            end
+function afficher(yN::Vector{Vector{T}}) where T<:Real
+    print("| ")
+    for i in 1:length(yN)
+        if yN[i][1] < 10
+            print("  ", yN[i][1], "  ")
+        elseif yN[i][1] < 100
+            print(" ", yN[i][1], "  ")
+        else
+            print(yN[i][1], "  ")
         end
-        print("|\n| ")
-        for i in 1:length(yN)
-            if yN[i].z[2] < 10
-                print("  ", yN[i].z[2], "  ")
-            elseif yN[i].z[2] < 100
-                print(" ", yN[i].z[2], "  ")
-            else
-                print(yN[i].z[2], "  ")
-            end
-        end
-        print("|\n")
-    else 
-        print("| ")
-        for i in 1:length(yN)
-            if yN[i][1] < 10
-                print("  ", yN[i][1], "  ")
-            elseif yN[i][1] < 100
-                print(" ", yN[i][1], "  ")
-            else
-                print(yN[i][1], "  ")
-            end
-        end
-        print("|\n| ")
-        for i in 1:length(yN)
-            if yN[i][2] < 10
-                print("  ", yN[i][2], "  ")
-            elseif yN[i][2] < 100
-                print(" ", yN[i][2], "  ")
-            else
-                print(yN[i][2], "  ")
-            end
-        end
-        print("|\n")
     end
+    print("|\n| ")
+    for i in 1:length(yN)
+        if yN[i][2] < 10
+            print("  ", yN[i][2], "  ")
+        elseif yN[i][2] < 100
+            print(" ", yN[i][2], "  ")
+        else
+            print(yN[i][2], "  ")
+        end
+    end
+    print("|\n")
 end
